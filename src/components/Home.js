e171a7c6c71c918c3d2c650af42d1af02538dc99
BunniMK <86545296+BunniMK@users.noreply.github.com>
2021-12-09 09:59:00 -0700

    Added styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -29,12 +29,14 @@ const Home = (props) => {
             <h1> Random Encounter </h1>
             <button onClick={handleClick}> New monster </button>
             <div> 
-                <h3> {monster.name} </h3>
-                <h4> Type: {monster.type}</h4>
-                <h4> Stats: </h4>
-                <h5> Size: {monster.size} </h5>
-                <h5> Armor Class: {monster.armor_class} </h5>
-                <h5> Hit points: {monster.hit_points} </h5>
+                <div className='stats'>
+                    <h3> {monster.name} </h3>
+                    <h4> Type: {monster.type}</h4>
+                    <h4> Stats: </h4>
+                    <h5> Size: {monster.size} </h5>
+                    <h5> Armor Class: {monster.armor_class} </h5>
+                    <h5> Hit points: {monster.hit_points} </h5>
+                </div>
             </div>
         </div>
     )

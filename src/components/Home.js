2b9d52223c0b599d0656df70c12d1b1a4bf0dcea
BunniMK <86545296+BunniMK@users.noreply.github.com>
2022-02-21 14:41:26 -0700

    removed comment

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ const Home = (props) => {
     useEffect(() => {
         props.aquireMonster();
     }, [])
-// this handles clicks
+
     const handleClick = () => {
         props.aquireMonster();
     }
